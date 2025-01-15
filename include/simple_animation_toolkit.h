a35904f07c8369792706743bb470540760e0c15f
Stretchyfish <m.maarssoe@gmail.com>
2025-01-15 07:47:05 +0100

    added easier to access library interface

---
+++ include/simple_animation_toolkit.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "simple_animation_toolkit/animation_engine.h"
+#include "simple_animation_toolkit/objects/animated_text_object.h"
+#include "simple_animation_toolkit/objects/animated_circle_object.h"
+#include "simple_animation_toolkit/objects/animated_square_object.h"
